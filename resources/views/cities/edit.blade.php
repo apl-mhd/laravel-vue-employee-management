780d9bf82bc463892582ac6664e5b65fbafee5f0
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2022-10-10 00:54:25 +0600

    city add done

---
+++ resources/views/cities/edit.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.main')
+@section('content')
+<div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">Cities</h1>
+</div>
+    
+  <div class="row">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Update Cities  ')}}
+                        <a href="{{route('cities.index')}}" class="float-right">Back</a>
+                    </div>
+    
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('cities.update', $city->id) }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="row mb-3">
+                                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('City Name') }}</label>
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name', $city->name) }}">
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+    
+                            <div class="row mb-3">
+                                <label for="state_id" class="col-md-4 col-form-label text-md-end">{{ __('State') }}</label>
+    
+                                <div class="col-md-6">
+                                    <select name="state_id" id="state_id" class=" form-control" aria-label="Default select example">
+                                        @foreach ($states as $state)
+                                            <option value="{{$state->id}}" {{$city->state_id ==  $state->id ? 'selected': ''}}>
+                                                {{$state->name}}
+                                            </option>
+                                        @endforeach
+                                    </select>
+    
+                                    @error('state_id')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+                         
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Store') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="m-2 p-2">
+                    <form method="POST" action="{{ route('cities.destroy', $city->id) }}">
+                        @csrf
+                        @method('DELETE')
+                        <button class="btn btn-danger">Delete {{$city->name}}</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+@endsection()
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Backend\ChangePasswordController;
+use App\Http\Controllers\Backend\CityController;
 use App\Http\Controllers\Backend\CountryController;
 use App\Http\Controllers\Backend\StateController;
 use Illuminate\Support\Facades\Route;
@@ -27,4 +28,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('users', UserController::class);
 Route::resource('countries', CountryController::class);
 Route::resource('states', StateController::class);
+Route::resource('cities', CityController::class);
 Route::post('users/{user}/change-password', [ChangePasswordController::class, 'change_password'])->name('users.change.password');
